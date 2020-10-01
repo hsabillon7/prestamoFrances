0879a0278bb5855e39e753b26a1d50091f585e23
Héctor Sabillón <h.hsabillon@gmail.com>
2020-09-30 20:45:40 -0600

    Implementación de express-handlebars como template engine y creación del endpoint de / con el formulario de préstamos

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 // Importar express
 const express = require("express");
+// Importar handlebars como template engine
+const exphbs = require("express-handlebars");
 
 // Crear un servidor express
 const app = express();
 
+// Indicar a express utilizar handlebars como template engine
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+
+app.set("view engine", "hbs");
+
 // Crear una ruta para /
 app.get("/", (req, res, next) => {
-  res.send("¡Bienvenido!");
+  res.render("formulario_prestamo");
 });
 
 // Inicializar el servidor en un puerto en específico
