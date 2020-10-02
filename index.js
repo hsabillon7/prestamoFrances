d0f51288dafb716a768c11381f7d639384a36992
Héctor Sabillón <h.hsabillon@gmail.com>
2020-10-01 20:53:45 -0600

    Implementación de bodyParser para obtener el cuerpo de las peticiones HTTP. Endpoint para el cálculo del préstamo.

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 const express = require("express");
 // Importar handlebars como template engine
 const exphbs = require("express-handlebars");
+// Importar body parser que nos permite acceder al cuerpo de la petición HTTP
+const bodyParser = require("body-parser");
+// Importar la función de cálculo de método francés
+const { calcularMetodoFrances } = require("./calculoMetodoFrances");
 
 // Crear un servidor express
 const app = express();
@@ -11,9 +15,24 @@ app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 
 app.set("view engine", "hbs");
 
+// Habilitar body parse para leer los datos del cuerpo de peticiones POST
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // Crear una ruta para /
+// Información sobre los verbos HTTP
+// https://developer.mozilla.org/es/docs/Web/HTTP/Methods
 app.get("/", (req, res, next) => {
   res.render("formulario_prestamo");
+});
+
+app.post("/prestamo", (req, res, next) => {
+  // Asignación por destructuring
+  // https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Operadores/Destructuring_assignment
+  const { monto, tasaInteres, periodo } = req.body;
+
+  calcularMetodoFrances(monto, tasaInteres, periodo);
+
+  res.send("Petición aceptada");
 });
 
 // Inicializar el servidor en un puerto en específico
