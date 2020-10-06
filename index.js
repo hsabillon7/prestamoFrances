c71536e0cef9b886fcb3fc5c2e131ffed55d0a4a
Héctor Sabillón <h.hsabillon@gmail.com>
2020-10-05 20:51:19 -0600

    El endpoint de /prestamo manda a llamar la vista para renderizar las cuotas del préstamo

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ app.post("/prestamo", (req, res, next) => {
   // https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Operadores/Destructuring_assignment
   const { monto, tasaInteres, periodo } = req.body;
 
-  calcularMetodoFrances(monto, tasaInteres, periodo);
+  const cuotas = calcularMetodoFrances(monto, tasaInteres, periodo);
 
-  res.send("Petición aceptada");
+  res.render("resultado_prestamo", { cuotas });
 });
 
 // Inicializar el servidor en un puerto en específico
