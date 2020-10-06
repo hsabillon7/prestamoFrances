8df02ebe71868d90b8c9bfa674f68a58f421859f
Héctor Sabillón <h.hsabillon@gmail.com>
2020-10-05 20:50:05 -0600

    Finalización de la función que calcula las cuotas del préstamo francés

--- calculoMetodoFrances.js
+++ calculoMetodoFrances.js
@@ -8,6 +8,7 @@ exports.calcularMetodoFrances = (capital, tasaInteres, plazo) => {
   let interes = 0;
   let tasaInteresDecimal = tasaInteres / 100;
   const cuotas = [];
+  let capitalAmortizar = 0;
 
   // Transformar los años en meses
   plazo = Math.round(plazo * 12);
@@ -15,7 +16,35 @@ exports.calcularMetodoFrances = (capital, tasaInteres, plazo) => {
   // Calcular la cuota
   cuota =
     (capital * tasaInteresDecimal) / (1 - (1 + tasaInteresDecimal) ** -plazo);
-
   cuota = cuota.toFixed(2);
-  console.log(cuota);
+
+  while (mes != plazo) {
+    // Obtener el interés por período
+    interes = capital * tasaInteresDecimal;
+    interes = interes.toFixed(2);
+
+    // El capital que se amortiza en la cuota
+    capitalAmortizar = cuota - interes;
+    capitalAmortizar = capitalAmortizar.toFixed(2);
+
+    // Actualizar el mes (período)
+    mes++;
+
+    // Actualizar la deuda
+    capital -= capitalAmortizar;
+    capital = capital.toFixed(2);
+
+    // TODO: Verificar los cálculos con valores decimales
+
+    // Almacenar los valores en el arreglo
+    cuotas.push({
+      mes,
+      capitalAmortizar,
+      interes,
+      cuota,
+      capital,
+    });
+  }
+
+  return cuotas;
 };
